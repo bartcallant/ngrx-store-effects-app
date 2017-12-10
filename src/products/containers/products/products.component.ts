98dba73e64c61dc70f36cbc60440005ac564f649
Bart Callant <bart@callant.net>
2017-12-10 12:35:33 +0100

    Finished 'State preload and protection via Guards'

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -32,7 +32,5 @@ export class ProductsComponent implements OnInit {
 
     ngOnInit() {
         this.pizzas$ = this.store.select(fromStore.getPizzasData);
-        this.store.dispatch(new fromStore.LoadPizzas());
-        this.store.dispatch(new fromStore.LoadToppings());
     }
 }
